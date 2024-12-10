b67c4413b42e8559a9953be0ac55f57d9197216e
김수진 <nuu042@naver.com>
2024-12-10 13:06:13 +0900

    Updates

--- static/js/690.7c980c0e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksujin_portf=self.webpackChunksujin_portf||[]).push([[690],{690:(s,e,t)=>{t.r(e),t.d(e,{default:()=>n});var u=t(579);const n=()=>(0,u.jsx)(u.Fragment,{})}}]);
-//# sourceMappingURL=690.7c980c0e.chunk.js.map
---
+++ static/js/984.c0fa3027.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksujin_portf=self.webpackChunksujin_portf||[]).push([[984],{984:(s,e,n)=>{n.r(e),n.d(e,{default:()=>u});var r=n(751),t=n(579);const u=()=>(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(r.dz,{}),(0,t.jsx)(r.wi,{})]})}}]);
+//# sourceMappingURL=984.c0fa3027.chunk.js.map
