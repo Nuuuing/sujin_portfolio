9fbbeb572ba253159eb49aadf48be86c0ffb13c3
김수진 <nuu042@naver.com>
2025-02-14 05:41:03 +0900

    [Feat] Spinner 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './assets/css/main.css';
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { MainRouter } from './routes';
+import { Spinner } from './components';
 
 function App() {
+
   return (
-    <Suspense fallback={<>LOADING</>}>
+    <Suspense fallback={<Spinner />}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <MainRouter />
       </BrowserRouter>
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export * from './Footer';
 export * from './DetailHeader';
 export * from './YoutubePlayer';
+export * from './Spinner'
