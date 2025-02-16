7b2d6793fd4e51826e8c09cb1cdaad483d86851a
김수진 <nuu042@naver.com>
2025-02-16 23:07:04 +0900

    Updates

---
+++ static/js/331.810c18ff.chunk.js
@@ -0,0 +1,10 @@
+"use strict";(self.webpackChunksujin_portf=self.webpackChunksujin_portf||[]).push([[331],{331:(e,t,s)=>{s.r(t),s.d(t,{default:()=>u});var r=s(43),i=s(216),n=s(733),a=s(770),d=s(464),c=s(960);const u=()=>{const{id:e}=(0,i.g)(),[t,s]=(0,r.useState)();return(0,r.useEffect)((()=>{s(e)}),[e]),(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(a.TF,{title:"Career"}),(0,c.jsx)(l,{children:(0,c.jsx)("div",{style:{width:"95%"},children:"0"===t?(0,c.jsx)(n.Xr,{}):(0,c.jsx)(c.Fragment,{})})})]})},l=d.Ay.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 4rem;
+    @media (max-width: 768px) {
+        margin-bottom: 2rem;
+    }
+`}}]);
+//# sourceMappingURL=331.810c18ff.chunk.js.map
