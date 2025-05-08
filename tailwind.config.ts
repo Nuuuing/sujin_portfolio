189fc03de8a4963625ce7941a7825701fbfa97c3
Sujin <nuu042@naver.com>
2025-05-08 17:55:23 +0900

    [Chore] postcss, tailwind, css 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,12 @@
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   darkMode: 'media',
+  content: [
+    './app/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -22,4 +29,6 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
+
+export default config;
