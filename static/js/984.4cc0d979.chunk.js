8e7dd82a916b3ab75e11450d75854446f7baae60
Sujin <nuu042@naver.com>
2025-05-30 16:48:14 +0900

    Updates

--- static/js/984.4cc0d979.chunk.js
+++
@@ -1,7 +0,0 @@
-"use strict";(self.webpackChunksujin_portf=self.webpackChunksujin_portf||[]).push([[984],{984:(e,s,i)=>{i.r(s),i.d(s,{default:()=>c});var l=i(733),d=i(464),n=i(960);const c=()=>(0,n.jsx)(f,{children:(0,n.jsx)(r,{children:(0,n.jsx)(l.dz,{})})}),f=d.Ay.div`
-    display: flex;
-    flex-direction: column;
-`,r=d.Ay.div`
-    flex: 1;
-`}}]);
-//# sourceMappingURL=984.4cc0d979.chunk.js.map
